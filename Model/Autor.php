d21fefa9a4e0ba0c1570d37b50935e7951087937
231052 <231052@CPD02-M17>
2025-08-11 15:55:01 -0300

    ajuste

--- Model/Autor.php
+++ Model/Autor.php
@@ -4,67 +4,110 @@
     use NovoPHPmain\DAO\AlunoDAO;
     use Exception;
 
-    final class Aluno extends Model
+   /**
+ * A camada model é responsável por transportar os dados da Controller até a DAO e vice-versa.
+ * Também é atribuído a Model a validação dos dados da View e controle de acesso aos métodos
+ * da DAO.
+ */
+final class Autor extends Model
+{
+    /**
+     * Declaração das propriedades conforme campos da tabela no banco de dados.
+     * para saber mais sobre Propriedades de Classe, leia: https://www.php.net/manual/pt_BR/language.oop5.properties.php
+     */
+    public ?int $Id = null;
+
+    public ?string $Nome
     {
-        public ?int $Id = null;
-
-        public ?string $Nome
+        set
         {
-            set
-            {
-                if(strlen($value) < 4)
-                    throw new Exception("Nome deve ter no mínimo 4 caracteres.");
+            if(strlen($value) < 3)
+                throw new Exception("Nome deve ter no mínimo 3 caracteres.");
 
-                    $this->Nome = $value;
-            }
-
-            get => $this->Nome ?? null;
+            $this->Nome = $value;
         }
 
-        public ?string $RA
-        {
-            set
-            {
-                if(empty($value))
-                    throw new Exception("Preencha o RA.");
-
-                    $this->RA ?? null;
-            }
-
-            get => $this->RA ?? null;
-        }
-
-        public ?string $Curso
-        {
-            set
-            {
-                if(strlen($value) < 4)
-                    throw new Exception("Curso deve ter no mínimo 4 caracteres.");
-
-                    $this->Curso = $value;
-            }
-
-            get => $this->Curso ?? null;
-        }
-
-        function save() : Aluno
-        {
-            return new AlunoDAO()->save($this);
-        }
-
-        function getById(int $id) : ?Aluno
-        {
-            return new AlunoDAO()->selectById($this);
-        }
-
-        function getAllRows() : array
-        {
-            $this->rows = new AlunoDAO()->select();
-        }
-
-        function delete(int $id) : bool
-        {
-            return new AlunoDAO()->delete($id);
-        }
+        get => $this->Nome ?? null;
     }
-?>
+
+
+    public ?string $Data_Nascimento
+    {
+        set
+        {
+            if(empty($value))
+                throw new Exception("Preencha a Data de Nascimento");
+
+            $this->Data_Nascimento = $value;
+        }
+
+        get => $this->Data_Nascimento ?? null;
+    }
+
+
+    public ?string $CPF
+    {
+        set
+        {
+            if(strlen($value) < 11)
+                throw new Exception("CPF deve ter no mínimo 11 caracteres.");
+
+            $this->CPF = $value;
+        }
+
+        get => $this->CPF ?? null;
+    }
+
+
+
+    
+    /**
+     * Declaração do método save que chamará a DAO para gravar no banco de dados
+     * o model preenchido.
+     */
+    function save() : Autor
+    {
+        /**
+         * Note que os objetos da classe AlunoDAO estão sendo criados de forma anônima.
+         * Fazemos isso quando podemos descartar o objeto logo apos o uso, ou seja,
+         * não sendo necessário armazenar o objeto em uma variável.
+         * Leia sobre: https://www.php.net/manual/pt_BR/language.oop5.anonymous.php
+         */
+        return new AutorDAO()->save($this);
+    }
+
+
+    /**
+     * Método que encapsula o acesso ao método selectById da camada DAO
+     * O método recebe um parâmetro do tipo inteiro que é o id do registro
+     * a ser recuperado do MySQL, via camada DAO.
+     */
+    function getById(int $id) : ?Autor
+    {
+        return new AutorDAO()->selectById($id);
+    }
+
+
+    /**
+     * Método que encapsula a chamada a DAO e que abastecerá a propriedade rows;
+     * Esse método é importante pois como a model é "vista" na View a propriedade
+     * $rows será acessada e possibilitará listar os registros vindos do banco de dados
+     */
+    function getAllRows() : array
+    {
+        $this->rows = new AutorDAO()->select();
+
+        return $this->rows;
+    }
+
+
+    /**
+     * Método que encapsula o acesso a DAO do método delete.
+     * O método recebe um parâmetro do tipo inteiro que é o id do registro
+     * que será excluido da tabela no MySQL, via camada DAO.
+     */
+    function delete(int $id) : bool
+    {
+        return new AutorDAO()->delete($id);
+    }
+}
--- Model/Livro.php
+++ Model/Livro.php
@@ -4,80 +4,126 @@
     use NovoPHPmain\DAO\LivroDao;
     use Exception;
 
-    final class Livro extends Model
+   /**
+ * A camada model é responsável por transportar os dados da Controller até a DAO e vice-versa.
+ * Também é atribuído a Model a validação dos dados da View e controle de acesso aos métodos
+ * da DAO.
+ */
+final class Livro extends Model
+{
+    /**
+     * Declaração das propriedades conforme campos da tabela no banco de dados.
+     * para saber mais sobre Propriedades de Classe, leia: https://www.php.net/manual/pt_BR/language.oop5.properties.php
+     */
+    public ?int $Id = null;
+
+    public array $rows_categorias = [];
+    public array $rows_autores = [];
+
+    public $Id_Categoria;
+
+    public $Id_Autores = [];
+
+    public ?string $Titulo
     {
-        public ?int $Id = null;
-
-        public ?string $Titulo
+        set
         {
-            set
-            {
-                if(strlen($value) < 1)
-                    throw new Exception("Título deve ter no mínimo 1 caracteres.");
+            if(strlen($value) < 3)
+                throw new Exception("Título deve ter no mínimo 3 caracteres.");
 
-                    $this->Titulo = $value;
-            }
-
-            get => $this->Nome ?? null;
+            $this->Titulo = $value;
         }
 
-        public ?int $Isbn
-        {
-            set
-            {
-                if(strlen($value) != 13)
-                    throw new Exception("ISBN deve conter 13 caracteres.");
-
-                    $this->Isbn = $value;
-            }
-
-            get => $this->Nome ?? null;
-        }
-
-        public ?string $Editora
-        {
-            set
-            {
-                if(strlen($value) < 1)
-                    throw new Exception("Editora deve ter no mínimo 1 caracteres.");
-
-                    $this->Editora = $value;
-            }
-
-            get => $this->Nome ?? null;
-        }
-
-        public ?string $Ano // Não sei se está certo
-        {
-            set
-            {
-                if($value == null)
-                    throw new Exception("Nome deve ter no mínimo 1 caracteres.");
-
-                    $this->Ano ?? null;
-            }
-
-            get => $this->Ano ?? null;
-        }
-
-        function save() : Livro
-        {
-            return new LivroDAO()->save($this);
-        }
-
-        function getById(int $id) : ?Aluno
-        {
-            return new LivroDAO()->selectById($id);
-        }
-
-        function getAllRows() 
-        {
-            $this->rows = new LivroDAO()->select();
-        }
-
-        function delete(int $id) : bool
-        {
-            return new LivroDAO()->delete($id);
-        }
+        get => $this->Titulo ?? null;
     }
-?>
+
+    public ?string $Isbn
+    {
+        set
+        {
+            if(strlen($value) < 3)
+                throw new Exception("ISBN deve ter no mínimo 3 caracteres.");
+
+            $this->Isbn = $value;
+        }
+
+        get => $this->Isbn ?? null;
+    }
+
+    public ?string $Editora
+    {
+        set
+        {
+            if(strlen($value) < 3)
+                throw new Exception("Editora deve ter no mínimo 3 caracteres.");
+
+            $this->Editora = $value;
+        }
+
+        get => $this->Editora ?? null;
+    }
+
+    public ?string $Ano
+    {
+        set
+        {
+            if(strlen($value) < 3)
+                throw new Exception("Ano deve ter no mínimo 3 caracteres.");
+
+            $this->Ano = $value;
+        }
+
+        get => $this->Ano ?? null;
+    }
+
+
+    /**
+     * Declaração do método save que chamará a DAO para gravar no banco de dados
+     * o model preenchido.
+     */
+    function save() : Livro
+    {
+        /**
+         * Note que os objetos da classe AlunoDAO estão sendo criados de forma anônima.
+         * Fazemos isso quando podemos descartar o objeto logo apos o uso, ou seja,
+         * não sendo necessário armazenar o objeto em uma variável.
+         * Leia sobre: https://www.php.net/manual/pt_BR/language.oop5.anonymous.php
+         */
+        return new LivroDAO()->save($this);
+    }
+
+
+    /**
+     * Método que encapsula o acesso ao método selectById da camada DAO
+     * O método recebe um parâmetro do tipo inteiro que é o id do registro
+     * a ser recuperado do MySQL, via camada DAO.
+     */
+    function getById(int $id) : ?Livro
+    {
+        return new LivroDAO()->selectById($id);
+    }
+
+
+    /**
+     * Método que encapsula a chamada a DAO e que abastecerá a propriedade rows;
+     * Esse método é importante pois como a model é "vista" na View a propriedade
+     * $rows será acessada e possibilitará listar os registros vindos do banco de dados
+     */
+    function getAllRows() : array
+    {
+        $this->rows = new LivroDAO()->select();
+
+        return $this->rows;
+    }
+
+
+    /**
+     * Método que encapsula o acesso a DAO do método delete.
+     * O método recebe um parâmetro do tipo inteiro que é o id do registro
+     * que será excluido da tabela no MySQL, via camada DAO.
+     */
+    function delete(int $id) : bool
+    {
+        return new LivroDAO()->delete($id);
+    }
+}
